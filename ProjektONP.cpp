b4753e63f06f3195e075d65bbaa389575fdc0d96
D3miksS <101121210+D3miksS@users.noreply.github.com>
2022-11-21 15:56:10 +0100

    Update ProjektONP.cpp

--- ProjektONP.cpp
+++ ProjektONP.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <string.h>
 #include <conio.h>
-#include <locale.h>
 using namespace std;
 
 // Dominik Ficek
@@ -13,11 +12,11 @@ void info();
 void instrukcja();
 
 /*
-	Funkcja sprawdza piorytety znaków i zapisuje do tablicy "piorytet"
+	Funkcja sprawdza piorytety znakĂłw i zapisuje do tablicy "piorytet"
 	in:
-		formula - wyrażenie podawane przez użytkownika
-		piorytet - tablica zawierająca piorytety znkaów z "formula"
-		ilZnak - zmienna licząca ilość znaków
+		formula - wyraĹĽenie podawane przez uĹĽytkownika
+		piorytet - tablica zawierajÄ…ca piorytety znkaĂłw z "formula"
+		ilZnak - zmienna liczÄ…ca iloĹ›Ä‡ znakĂłw
 	out:
 		NULL
 */
@@ -36,18 +35,18 @@ void check_priority(string& formula, short* piorytet, short& ilZnak) {
 		else if (formula[i] == '*' || formula[i] == '/') {
 			piorytet[i] = 2 + d;
 			ilZnak++;
-		};
-	};
-};
+		}
+	}
+}
 
 /*
 	Funkcja szuka maksimum w tavlicy "piorytet"
 	in:
-		formula - wyrażenie podawane przez użytkownika
-		piorytet - tablica zawierająca piorytety znkaów z "formula"
-		maxId - zmienna przechowująca nr pozycji największej liczby z tablicy "piorytet"
+		formula - wyraĹĽenie podawane przez uĹĽytkownika
+		piorytet - tablica zawierajÄ…ca piorytety znkaĂłw z "formula"
+		maxId - zmienna przechowujÄ…ca nr pozycji najwiÄ™kszej liczby z tablicy "piorytet"
 	out:
-		maxId - zwaraca zmienną z nr pozycji największej liczby z tablicy "piorytet"
+		maxId - zwaraca zmiennÄ… z nr pozycji najwiÄ™kszej liczby z tablicy "piorytet"
 */
 int search_max(string& formula, short* piorytet, short& maxId) {
 	int max = 0;
@@ -55,31 +54,31 @@ int search_max(string& formula, short* piorytet, short& maxId) {
 		if (piorytet[j] > max) {
 			max = piorytet[j];
 			maxId = j;
-		};
-	};
+		}
+	}
 	return maxId;
-};
+}
 
 //stos
 /*
-	Fuckcja odczytująca aktualną daną ze stosu w celu sprawdzenia czy jest pełny
+	Fuckcja odczytujÄ…ca aktualnÄ… danÄ… ze stosu
 	in:
-		pp - flaga informująca o aktualnym położeniu n stosie
+		pp - flaga informujÄ…ca o aktualnym poĹ‚oĹĽeniu n stosie
 	out:
-		true - stos jest pełny
-		false - stos nie jest pełny
+		true - stos jest peĹ‚ny
+		false - stos nie jest peĹ‚ny
 */
 bool czy_pelny(short& pp) {
 	if (pp == 9)
 		return true;
 	else
 		return false;
-};
+}
 
 /*
-	Fuckcja odczytująca aktualną daną ze stosu w celu sprawdzenia czy jest pusty
+	Fuckcja odczytujÄ…ca aktualnÄ… danÄ… ze stosu
 	in:
-		pp - flaga informująca o aktualnym położeniu n stosie
+		pp - flaga informujÄ…ca o aktualnym poĹ‚oĹĽeniu n stosie
 	out:
 		true - stos jest pusty
 		false - stos nie jest pusty
@@ -89,16 +88,16 @@ bool czy_pusty(short& pp) {
 		return true;
 	else
 		return false;
-};
+}
 
 /*
-	Fuckcja dodająca na stos
+	Fuckcja dodajÄ…ca na stos
 	in:
 		nn - dana do dopisania na stos
-		pp - flaga informująca o aktualnym położeniu n stosie
+		pp - flaga informujÄ…ca o aktualnym poĹ‚oĹĽeniu n stosie
 		stoss - stos
 	out:
-		'a' - kod błędu oznaczjący, że stos jest pełny
+		'a' - kod bĹ‚Ä™du oznaczjÄ…cy, ĹĽe stos jest peĹ‚ny
 */
 void na_stos(double& nn, short& pp, double* stoss) {
 	if (!czy_pelny(pp)) {
@@ -107,16 +106,16 @@ void na_stos(double& nn, short& pp, double* stoss) {
 	}
 	else
 		throw 'a';
-};
+}
 
 /*
-	Fuckcja odczytująca aktualną daną ze stosu w celu zdjęcia jej ze stosu
+	Fuckcja odczytujÄ…ca aktualnÄ… danÄ… ze stosu
 	in:
-		pp - flaga informująca o aktualnym położeniu n stosie
+		pp - flaga informujÄ…ca o aktualnym poĹ‚oĹĽeniu n stosie
 		stoss - stos
 	out:
-		z - dana z góry stosu
-		'b' - kod błędu oznaczający, że stos jest pusty
+		z - dana z gĂłry stosu
+		'b' - kod bĹ‚Ä™du oznaczajÄ…cy, ĹĽe stos jest pusty
 */
 double ze_stosu(short& pp, double* stoss) {
 	double z;
@@ -127,16 +126,16 @@ double ze_stosu(short& pp, double* stoss) {
 	}
 	else
 		throw 'b';
-};
+}
 
 /*
-	Fuckcja odczytująca aktualną daną ze stosu
+	Fuckcja odczytujÄ…ca aktualnÄ… danÄ… ze stosu
 	in:
-		pp - flaga informująca o aktualnym położeniu n stosie
+		pp - flaga informujÄ…ca o aktualnym poĹ‚oĹĽeniu n stosie
 		stoss - stos
 	out:
-		stos[pp] - dana z góry stosu
-		'b' - kod błędu oznaczający, że stos jest pusty
+		stos[pp] - dana z gĂłry stosu
+		'b' - kod bĹ‚Ä™du oznaczajÄ…cy, ĹĽe stos jest pusty
 */
 double zobacz(short& pp, double* stoss) {
 	if (!czy_pusty(pp)) {
@@ -144,24 +143,43 @@ double zobacz(short& pp, double* stoss) {
 	}
 	else
 		throw 'b';
-};
+}
 // koniec stosu
 
 /*
-	Funkcja zapisująca wyrażenie podane przez użytkownika w postacji ONP
+	Funkcja sprawdzajÄ…ca poprawnoĹ›Ä‡ wpisanych znakĂłw. Dobre tylko znaki dziaĹ‚aĹ„ matematycznych i cyfry
 	in:
-		maxId - numer pozycji najważniejszego znaku
-		onp - tablica, w której zapisywane będzie wyrażenie ONP
-		formula - wyrażenie podane przez użytkownika
-		piorytety - tablica przechowująca wartości ważności kolejnych znaków wyrażenia
+		formula - wyraĹĽenie podane przez urzytkownika, do sprawdzenia
+		jj - pozycja znaku do sprawdzenia
+	out:
+		true - wpisany znak poprawny
+		false - wpisany znak bĹ‚Ä™dny
+*/
+bool czy_liczba(string& formula, short& jj) {
+	if ((formula[jj] > 47) && (formula[jj] < 58))
+		return true;
+	else if (formula[jj] == '+' || formula[jj] == '-' || formula[jj] == '/' || formula[jj] == '*')
+		return true;
+	else
+		return false;
+}
+
+/*
+	Funkcja zapisujÄ…ca wyraĹĽenie podane przez uĹĽytkownika w postacji ONP
+	in:
+		maxId - numer pozycji najwaĹĽniejszego znaku
+		onp - tablica, w ktĂłrej zapisywane bÄ™dzie wyraĹĽenie ONP
+		formula - wyraĹĽenie podane przez uĹĽytkownika
+		piorytety - tablica przechowujÄ…ca wartoĹ›ci waĹĽnoĹ›ci kolejnych znakĂłw wyraĹĽenia
 		onpI - wolna pozycja do zapisu w tablicy onp
-		ujemna - tablica przechowująca, na której pozycji są liczby ujemne
-		ujemnaOnp - tablica przechowująca, na której pozycji w zapisie ONP jest liczba ujemna
+		ujemna - tablica przechowujÄ…ca, na ktĂłrej pozycji sÄ… liczby ujemne
+		ujemnaLicz - zmienna przesuwajÄ…ca pozycje w tablicy "ujemne ONP"
+		ujemnaOnp - tablica przechowujÄ…ca, na ktĂłrej pozycji w zapisie ONP jest liczba ujemna
 	out:
 		NULL
 */
-void add_to_onp(short maxId, char* onp, string& formula, short* piorytety, short& onpI, short* ujemna, short* ujemnaOnp) {
-	/*maxId = search_max(formula, piorytety, maxId);
+void add_to_onp(short maxId, char* onp, string& formula, short* piorytety, short& onpI, short* ujemna, short& ujemnaLicz, short* ujemnaOnp) {
+	maxId = search_max(formula, piorytety, maxId);
 	short id1 = maxId + 1;
 	short id2 = maxId - 1;
 
@@ -209,68 +227,16 @@ void add_to_onp(short maxId, char* onp, string& formula, short* piorytety, short
 		onp[onpI] = formula[maxId];
 		onpI++;
 		ujemnaLicz++;
-	};
-	piorytety[maxId] = -1;*/
-
-	maxId = search_max(formula, piorytety, maxId);
-	int id1 = maxId + 1;
-
-	//dodaj wszystko z lewej do znaku
-	for (int i = 0; i < maxId; i++) {
-		if (((formula[i] > 47) && (formula[i] < 58)) && (piorytety[i] >= 0 || piorytety[i] == -3)) {
-			if (ujemna[i] == -3) {
-				ujemnaOnp[onpI] = -2;
-			};
-			onp[onpI] = formula[i];
-			onpI++;
-			piorytety[i] = -1;
-		}
-		else if (formula[i] == '(') {
-			if (formula[i + 1] == '-') {
-				if (ujemna[i + 2] == -2 && piorytety[i + 2] >= 0) {
-					ujemnaOnp[onpI] = -2;
-					onp[onpI] = formula[i + 2];
-					onpI++;
-					piorytety[i + 2] = -1;
-				};
-			};
-		};
-	};
-
-	// dodaj jednÄ… z prawej od znaku
-	if (((formula[id1] > 47) && (formula[id1] < 58)) && piorytety[id1] >= 0) {
-		if (ujemna[maxId] == -3) {
-			ujemnaOnp[onpI] = -2;
-		};
-		onp[onpI] = formula[id1];
-		onpI++;
-		piorytety[id1] = -1;
 	}
-	else if (formula[id1] == '(') {
-		if (formula[id1 + 1] == '-') {
-			if (ujemna[id1 + 2] == -2 && piorytety[id1 + 2] >= 0) {
-				ujemnaOnp[onpI] = -2;
-				onp[onpI] = formula[id1 + 2];
-				onpI++;
-				piorytety[id1 + 2] = -1;
-			};
-		};
-	};
-
-	// sprawdza i dodaj znak
-	if (ujemna[maxId] != -3 && piorytety[maxId] != -1) {
-		onp[onpI] = formula[maxId];
-		onpI++;
-	};
 	piorytety[maxId] = -1;
-};
+}
 
 /*
-	Funkcja obliczająca wynik działania dwóch liczb
+	Funkcja obliczajÄ…ca wynik dziaĹ‚ania dwĂłch liczb
 	in:
-		onp - wyrażenie zapisane za pomocą sposobu ONP
+		onp - wyraĹĽenie zapisane za pomocÄ… sposobu ONP
 		ii - aktualna pozycja w tablicy "onp"
-		wynik - zmienna przechowująca wynik działania
+		wynik - zmienna przechowujÄ…ca wynik dziaĹ‚ania
 	out:
 		NULL
 */
@@ -297,19 +263,19 @@ void dzialanie(char* onp, double* stos, short& ii, double& wynik, short& p) {
 		break;
 	default:
 		break;
-	};
-};
+	}
+}
 
 /*
-	Funkcja obliczająca działanie na stosie
+	Funkcja obliczajÄ…ca dziaĹ‚anie na stosie
 	in:
-		onp - wyrażenie zapisane w postaci onp
-		onpI - pozycja aktualnej cyfry lub znaku branego do obliczeń
-		stos - tablica przechowująca dane do aktualnych obliczeń
+		onp - wyraĹĽenie zapisane w postaci onp
+		onpI - pozycja aktualnej cyfry lub znaku branego do obliczeĹ„
+		stos - tablica przechowujÄ…ca dane do aktualnych obliczeĹ„
 		p - aktualna pozycja na stosie
-		ujemnaOnp - tablica przechowująca, na której pozycji w zapisie ONP jest liczba ujemna
+		ujemnaOnp - tablica przechowujÄ…ca, na ktĂłrej pozycji w zapisie ONP jest liczba ujemna
 	out:
-		wynik - wynik obliczanego wyrażenia
+		wynik - wynik obliczanego wyraĹĽenia
 */
 double licz_wyraz(char* onp, short& onpI, double* stos, short& p, short* ujemnaOnp) {
 	double wynik = 0;
@@ -318,18 +284,18 @@ double licz_wyraz(char* onp, short& onpI, double* stos, short& p, short* ujemnaO
 			double cyfra = onp[i] - 48;
 			if (ujemnaOnp[i] == -2) {
 				cyfra = cyfra * -1;
-			};
+			}
 			na_stos(cyfra, p, stos);
 		}
 		else if ((onp[i] == '+' || onp[i] == '-' || onp[i] == '/' || onp[i] == '*') && ujemnaOnp[i] != -3) {
 			dzialanie(onp, stos, i, wynik, p);
-		};
-	};
+		}
+	}
 	return wynik;
-};
+}
 
 /*
-	Funkcja odpowiadająca za całe podanie i obliczenie wyrażenia
+	Funkcja odpowiadajÄ…ca za caĹ‚e podanie i obliczenie wyraĹĽenia
 	in:
 		NULL
 	out:
@@ -346,49 +312,50 @@ void program() {
 	short ilZnak = 0;
 	short ujemna[100] = { 0 };
 	short ujemnaOnp[100] = { 0 };
+	short ujemnaLicz = 0;
 
-	cout << "Podaj równanie.\n";
+	cout << "Podaj rĂłwnanie.\n";
 	cin >> formula;
 
 	check_priority(formula, piorytet, ilZnak);
 	check_ujemna(formula, ujemna, piorytet);
 
 	while (ilZnak > 0) {
-		add_to_onp(maxId, onp, formula, piorytet, onpI, ujemna, ujemnaOnp);
+		add_to_onp(maxId, onp, formula, piorytet, onpI, ujemna, ujemnaLicz, ujemnaOnp);
 		ilZnak--;
 	};
 
-	cout << "Wyrażenie w postaci ONP: ";
+	cout << "WyraĹĽenie w postaci ONP: ";
 	for (int i = 0; i < formula.length(); i++) {
 		if (ujemnaOnp[i] == -2)
 			cout << (onp[i] - 48) * -1 << " ";
 		else
 			cout << onp[i] << " ";
-	};
+	}
 
 	try {
 		cout << "\nWynik to: " << licz_wyraz(onp, onpI, stos, p, ujemnaOnp) << endl;
 	}
 	catch (char numError) {
 		if (numError == 'a') {
-			cout << "\n---Stos jest pełny---\n";
+			cout << "\n---Stos jest peĹ‚ny---\n";
 		}
 		else if (numError == 'b') {
 			cout << "\n---Stos jest pusty---\n";
-		};
-	};
-};
+		}
+	}
+}
 
 /*
-	Funcja odpowiadająca za UI
+	Funcja odpowiadajÄ…ca za UI
 	in:
-		nr - zmienna przechuwująca podany nr opcji przez użytkownika
+		nr - zmienna przechuwujÄ…ca podany nr opcji przez uĹĽytkownika
 	out:
 		NULL
 */
 void menu(short& nr) {
 	if (nr == 0) {
-		cout << "1. Wpisz działanie do obiczenia\n2. Instrukcja\n3. Info\n4. Zakończ program\n";
+		cout << "1. Wpisz dziaĹ‚anie do obiczenia\n2. Instrukcja\n3. Info\n4. ZakoĹ„cz program\n";
 		pod_nr_menu(nr, 0);
 	};
 
@@ -413,19 +380,19 @@ void menu(short& nr) {
 		break;
 	default:
 		break;
-	};
-};
+	}
+}
 
 /*
-	Funkcja odpowiadająca za menu powrotu
+	Funkcja odpowiadajÄ…ca za menu powrotu
 	in:
-		nr - zmienna przechuwująca podany nr opcji przez użytkownika
+		nr - zmienna przechuwujÄ…ca podany nr opcji przez uĹĽytkownika
 	out:
 		NULL
 */
 void menu_back(short& nr) {
 	if (nr == 49) {
-		cout << "----------\n1. Wróć do menu głównego\n2. Zakończ program\n3. Powtórz\n";
+		cout << "----------\n1. WrĂłÄ‡ do menu gĹ‚Ăłwnego\n2. ZakoĹ„cz program\n3. PowtĂłrz\n";
 		pod_nr_menu(nr, 1);
 		switch (nr)
 		{
@@ -444,10 +411,10 @@ void menu_back(short& nr) {
 			break;
 		default:
 			break;
-		};
+		}
 	}
 	else {
-		cout << "----------\n1. Wróć do menu głównego\n2. Zakończ program\n";
+		cout << "----------\n1. WrĂłÄ‡ do menu gĹ‚Ăłwnego\n2. ZakoĹ„cz program\n";
 		pod_nr_menu(nr, 2);
 		switch (nr)
 		{
@@ -461,15 +428,15 @@ void menu_back(short& nr) {
 			break;
 		default:
 			break;
-		};
-	};
-};
+		}
+	}
+}
 
 /*
-	Funkcja prosząca o wybranie opcji menu i sprwadzająca poprawność wybieranej opcji menu
+	Funkcja proszÄ…ca o wybranie opcji menu i sprwadzajÄ…ca poprawnoĹ›Ä‡ wybieranej opcji menu
 	in:
-		nr - zmienna przechowująca nr opcji w menu
-		a - zmienna kontrolująca z którego menu przychodzimy
+		nr - zmienna przechowujÄ…ca nr opcji w menu
+		a - zmienna kontrolujÄ…ca z ktĂłrego menu przychodzimy
 	out:
 		nr - zwraca poprawny nr opcji menu
 */
@@ -479,65 +446,66 @@ short pod_nr_menu(short& nr, short a) {
 		if (nr >= 49 && nr <= 52)
 			return nr;
 		else {
-			cout << "\nZły numer opcji\n";
+			cout << "\nZĹ‚y numer opcji\n";
 			pod_nr_menu(nr, 0);
-		};
+		}
 	}
 	else if (a == 1) {
 		if (nr >= 49 && nr <= 51)
 			return nr;
 		else {
-			cout << "\nZły numer opcji\n";
+			cout << "\nZĹ‚y numer opcji\n";
 			pod_nr_menu(nr, 1);
-		};
+		}
 	}
 	else {
 		if (nr >= 49 && nr <= 50)
 			return nr;
 		else {
-			cout << "\nZły numer opcji\n";
+			cout << "\nZĹ‚y numer opcji\n";
 			pod_nr_menu(nr, 2);
-		};
-	};
-};
+		}
+	}
+}
 
 /*
-	Funkcja sprawdzająca czy dana liczba jest ujemna
+	Funkcja sprawdzajÄ…ca czy dana liczba jest ujemna
 	in:
-		formula - ciąg podany przez urzytkownika
-		ujemna - tablica przechowująca, na któej pozycji są liczby ujemne i znaki ujemności
-		piorytety - tablica piorytetów wykonywania działań
+		formula - ciÄ…g podany przez urzytkownika
+		ujemna - tablica przechowujÄ…ca, na ktĂłej pozycji sÄ… liczby ujemne i znaki ujemnoĹ›ci
+		piorytety - tablica piorytetĂłw wykonywania dziaĹ‚aĹ„
 */
 void check_ujemna(string& formula, short* ujemna, short* piorytety) {
 	for (int i = 0; i < formula.length(); i++) {
 		if (formula[i] == '(') {
 			if (formula[i + 1] == '-') {
-				int jj = i + 2;
-				if ((formula[jj] > 47) && (formula[jj] < 58)) {
+				short jj = i + 2;
+				if (czy_liczba(formula, jj)) {
 					ujemna[i + 1] = -3;
 					piorytety[i + 1] = 1;
 					ujemna[i + 2] = -2;
-				};
-			};
-		};
-	};
-};
+				}
+			}
+		}
+	}
+}
 
 /*
-	Funkcja wyświetlająca inforamcje o autorze
+	Funkcja wyĹ›wietlajÄ…ca inforamcje o autorze
 */
 void info() {
-	cout << "Autor: Dominik Ficek\nMUP Oświęcim\nInformatyka Rok I\nData: 2022\nWersja: 0.95\n";
-};
+	char copyr = 169;
+	cout << "Autor: Dominik Ficek\nMUP OĹ›wiÄ™cim\nInformatyka Rok I\nData: 2022\nWersja: 0.9\n";
+}
 
 /*
-	Funkcja wyświetlająca instrukcje do programu
+	Funkcja wyĹ›wietlajÄ…ca instrukcje do programu
 */
 void instrukcja() {
 	cout << "-----Instrukcja-----\nDozwolone znaki do wpisania:\n# cyfry od 0 do 9\n# +, -, *, /, (, )\n\nZapis liczb ujemnych: (-3), (-8)\n\nPodstawowe zasady:\n";
-	cout << "# W nawiasach nie może być samej cyfry\n# Przed nawiasem nie może znajdować sie znak bez poprzedzającej go cyfry\n# Tylko wyżej wymienione znaki gwarantują poprawoność wykonania działania\n\n";
-	cout << "Przykładowe wyrażenie do obliczenia:\n(-2)*4-((5/2)*(-2))\n\nZapisy niedozwolone:\n# -(2*3)\n# a+2\n# 2-(3)\n\n";
-};
+	cout << "# W nawiasach nie moĹĽe byÄ‡ samej cyfry\n# Przed nawiasem nie moĹĽe znajdowaÄ‡ sie znak bez poprzedzajÄ…cej go cyfry\n# Tylko wyĹĽej wymienione znaki gwarantujÄ… poprawoĹ›Ä‡ wykonania dziaĹ‚ania\n\n";
+	cout << "PrzykĹ‚adowe wyraĹĽenie do obliczenia:\n(-2)*4-((5/2)*(-2))\n\nZapisy niedozwolone:\n# -(2*3)\n# a+2\n# 2-(3)\n\n";
+}
 
 int main(void) {
 	setlocale(LC_CTYPE, "Polish");	// Polskie znaki w Ĺ›rodku kodu
@@ -546,4 +514,4 @@ int main(void) {
 	menu(nr);
 
 	return 0;
-};
+}
